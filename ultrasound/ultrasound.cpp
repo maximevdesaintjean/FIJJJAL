023a4e7fe2de0542d27857edf1c7d6fcc06a3b10
JonathanPetit <petit.jonathan16@gmail.com>
2019-04-26 15:50:11 +0200

    Comment ultrasound part

--- ultrasound/ultrasound.cpp
+++ ultrasound/ultrasound.cpp
@@ -6,11 +6,13 @@
 
 using namespace std;
 
+/* Constructor */
 Ultrasound::Ultrasound(int echo, int trig) {
     this->echo = echo;
     this->trig = trig;
 }
 
+/* Initialise pin for ultrasound */
 void Ultrasound::initialize()
 {
     gpioSetMode(this->trig, PI_OUTPUT); 
@@ -19,10 +21,12 @@ void Ultrasound::initialize()
     gpioSleep(PI_TIME_RELATIVE, 0.5, 0);
 }
 
+/* Get distance for object detection */
 double Ultrasound::getDistance()
 {
 	end = 0;
 
+    /* To be sure that trig pin is on low level */
 	gpioWrite(this->trig, 1);
 	gpioDelay(10);
 	gpioWrite(this->trig, 0);
@@ -32,7 +36,9 @@ double Ultrasound::getDistance()
         Ultrasound::pulse();
 	}
 
+    /* Calcul distance with time difference between end and start pulse */
     long difference_time = end - start;
+    /* Convert time to distance */
     double distance = (double)difference_time/58.7734;
 	
 	if (end != 0)
@@ -41,6 +47,7 @@ double Ultrasound::getDistance()
 		return 0;
 }
 
+/* Function to get pulse between echo trig to recover time dor distance */
 void Ultrasound::pulse()
 {
     start = gpioTick();
