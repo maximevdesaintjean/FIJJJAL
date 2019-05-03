291dbd8568e13ba16da7a11cdb44480e4405f312
JonathanPetit <petit.jonathan16@gmail.com>
2019-05-03 15:56:51 +0200

    update main and gitignore

--- fijjal.h
+++ fijjal.h
@@ -14,6 +14,20 @@ class Fijjal {
         /* Ultrasound 2 */
         int ultra_echo2 = 26;
         int ultra_trig2 = 19;
+
+        /* Ultrasound 3 */
+        int ultra_echo3 = 0;
+        int ultra_trig3 = 0;
+
+        /* Ultrasound 4 */
+        int ultra_echo4 = 0;
+        int ultra_trig4 = 0;
+
+        /* Motors */
+        int motor_pin1 = 0;
+        int motor_pin2 = 0;
+        int motor_pin3 = 0;
+        int motor_pin4 = 0;
 };
 
 #endif
--- main.cpp
+++ main.cpp
@@ -4,7 +4,9 @@
 #include "./motor/motor.h"
 #include "./pid/pid.h"
 #include "./ultrasound/ultrasound.h"
+#include "./gyro/gyro.h"
 #include "fijjal.h"
+
 
 int main() {
     /* Initialise GPIO pin */
@@ -20,6 +22,10 @@ int main() {
     /* Initialize Ultrasound */
     Ultrasound ultra1 = Ultrasound(f.ultra_echo1, f.ultra_trig1);
     Ultrasound ultra2 = Ultrasound(f.ultra_echo2, f.ultra_trig2); 
+
+    /* Initialize Gyroscope */
+    Gyro gyro = Gyro();
+    gyro.initialise();
 
     return 0;
 }
